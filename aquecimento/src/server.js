6f023f865fb3837a35981914d58ad1b50aed069f
maicom goncalves <maicom.lithiun@yahoo.com.br>
2020-03-01 14:42:39 -0400

    correção de erros

--- aquecimento/src/server.js
+++ aquecimento/src/server.js
@@ -1,9 +1,12 @@
-console.log(('Servidor Iniciado'));
+const app = require('./app');
 
-const app = require ('./app');
+/*
 let porta = 3000;
-
-if(process.env.PORT){
-    porta=process.env.PORT;
+if (process.env.PORT) {
+    porta = process.env.PORT;
 }
-app.listen(porta, () => console.log(`App ouvindo na porta ${porta}`));
+*/
+const porta = process.env.PORT || 3000;
+
+app.listen(porta,
+    () => console.log(`App ouvindo na porta ${porta}`));
